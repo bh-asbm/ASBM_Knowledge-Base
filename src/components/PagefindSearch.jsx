f730d0ec51855ad53af2601341b75b48c0634b3a
bh-asbm <129250905+bh-asbm@users.noreply.github.com>
2025-10-02 20:51:52 -0400

    Create PagefindSearch component

---
+++ src/components/PagefindSearch.jsx
@@ -0,0 +1,19 @@
+import React, { useEffect } from "react";
+
+export default function PagefindSearch() {
+  useEffect(() => {
+    const script = document.createElement("script");
+    script.src = "/pagefind/pagefind.js";
+    script.onload = () => {
+      if (window.pagefindUI) {
+        window.pagefindUI({ element: "#search" });
+      }
+    };
+    document.body.appendChild(script);
+    return () => {
+      document.body.removeChild(script);
+    };
+  }, []);
+
+  return <div id="search"></div>;
+}
