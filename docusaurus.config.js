b932a0a0bd62344462976d5e135bf0afc2ad6ed9
bh-asbm <129250905+bh-asbm@users.noreply.github.com>
2025-10-07 00:00:30 -0400

    Update docusaurus.config.js

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -32,7 +32,7 @@ module.exports = {
       items: [
         { to: '/docs/quick-guides/choosing-a-sealer', label: 'Quick Guides', position: 'left' },
         { to: '/docs/vendors/euclid-chemical', label: 'Vendors', position: 'left' },
-        { href: 'https://github.com/bh-asbm/<repo>', label: 'GitHub', position: 'right' }
+        { href: 'https://github.com/bh-asbm/ASBM_Knowledge-Base', label: 'GitHub', position: 'right' }
       ]
     },
     prism: { additionalLanguages: ['bash','json','yaml'] }
